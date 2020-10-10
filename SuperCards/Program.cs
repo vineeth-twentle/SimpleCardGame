b310deec163b95ebe9be317c9983700f44c382dd
vineeth-twentle <65825526+vineeth-twentle@users.noreply.github.com>
2020-10-10 12:52:11 +0530

    Add files via upload
    Add program file

---
+++ SuperCards/Program.cs
@@ -0,0 +1,14 @@
+﻿using SuperCards.BusinessLogic;
+
+namespace SuperCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates the object of game for simplecard type.
+            SimpleCardGame simpleCardGame = new SimpleCardGame();
+            simpleCardGame.Start();
+        }
+    }
+}
